1bdf1007e247e03e791473d5c6026723f037b2e4
Taylor Oliveira <uaitayloroliveira@gmail.com>
2020-09-15 22:15:19 -0300

    0001# Estrutura Inicial
    0001# Estrutura Inicial

---
+++ src/main/java/com/eldorado/appointment/EldoradoAppointmentApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldoradoAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldoradoAppointmentApplication.class, args);
+	}
+
+}
